3c71f21235dd3f0bd74d9dbc6ea25615cc3f5eab
Sanika Suryawanshi <sanikasuryawanshi0305@gmail.com>
2022-09-07 06:00:14 +0000

    Added title and buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import React, {useState} from "react";
 import "./styles.css";
 
 export default function App() {
+    
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>🍔 goodFood</h1>
+      
+      <small>Check out my ratings for different kinds of food. Select a 
+          genre to get started</small>
+      
+      <div>
+        <button style={{padding:"0.5rem", backgroundColor:"rgb(210, 170, 118)", borderRadius:"0.5rem", width:"7rem", margin: "1rem", cursor:"pointer"}}>Italian</button>
+        
+        <button style={{padding:"0.5rem", backgroundColor:"rgb(210, 170, 118)", borderRadius:"0.5rem", width:"7rem", margin: "1rem", cursor:"pointer"}}>Indian</button>
+        
+        <button style={{padding:"0.5rem", backgroundColor:"rgb(210, 170, 118)", borderRadius:"0.5rem", width:"7rem", margin: "1rem", cursor:"pointer"}}>Chinese</button>
+        </div>
     </div>
   );
 }
